d270833c072e785d9fc15c854f96c4d6cb34fca0
Naoki Okumura <naoki.okumura@gmail.com>
2025-09-18 19:10:12 +0900

    バグ修正

--- src/main/java/okurun/driver/action/AvoidWallDriveAction.java
+++ src/main/java/okurun/driver/action/AvoidWallDriveAction.java
@@ -24,20 +24,20 @@ public class AvoidWallDriveAction extends AbstractDriveAction {
         final IBot bot = commander.getBot();
         final ArenaMap arenaMap = ArenaMap.getInstance();
         final Wall nearestWall = arenaMap.getNearestWall(bot);
-        final double wallDegree = nearestWall.degreeTo(bot);
+        final double degreeToWall = nearestWall.degreeTo(bot);
         double turnDegree;
         double forwardDistance = 30;
         double speed = Constants.MAX_SPEED;
         final double wallEscapeAngleOffset = 30;
-        if (wallDegree == 0) {
+        if (degreeToWall == 0) {
             turnDegree = 90 + wallEscapeAngleOffset;
         } else {
-            final Direction direction = (wallDegree < 0) ? Direction.LEFT : Direction.RIGHT;
+            final Direction direction = (degreeToWall < 0) ? Direction.LEFT : Direction.RIGHT;
             if (bot.getSpeed() < 0) {
                 forwardDistance = -forwardDistance;
-                turnDegree = bot.normalizeRelativeAngle((90 + Math.abs(wallDegree) + wallEscapeAngleOffset) * direction.value);
+                turnDegree = bot.normalizeRelativeAngle((90 + Math.abs(degreeToWall) + wallEscapeAngleOffset) * direction.value);
             } else {
-                turnDegree = bot.normalizeRelativeAngle((90 - Math.abs(wallDegree) + wallEscapeAngleOffset) * direction.value);
+                turnDegree = bot.normalizeRelativeAngle((90 - Math.abs(degreeToWall) + wallEscapeAngleOffset) * direction.value);
             }
         }
 
@@ -51,11 +51,9 @@ public class AvoidWallDriveAction extends AbstractDriveAction {
                 if (distanceToEnemy < 100) {
                     final double degreeToEnemy = bot.bearingTo(predictedEnemy.x, predictedEnemy.y);
                     if (forwardDistance > 0 && Math.abs(degreeToEnemy) < 45) {
-                        forwardDistance = -forwardDistance;
-                        turnDegree = -turnDegree;
+                        turnDegree = bot.normalizeRelativeAngle(degreeToWall + 180);
                     } else if (forwardDistance < 0 && Math.abs(degreeToEnemy) > 135) {
-                        forwardDistance = -forwardDistance;
-                        turnDegree = -turnDegree;
+                        turnDegree = degreeToWall;
                     }
                 }
             }
